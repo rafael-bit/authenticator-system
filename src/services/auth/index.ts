c8b071f76f287e8ae7b80e45070d2252336c4751
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-10-15 17:21:54 -0300

    Magic Link with Resend

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
-
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '../database';
+import { sendVerificationRequest } from '../util';
 
 export const {
 	handlers: { GET, POST },
@@ -20,7 +20,8 @@ export const {
 	providers: [
 		EmailProvider({
 			server: process.env.EMAIL_SERVER,
-			from: process.env.EMAIL_FROM
+			from: process.env.EMAIL_FROM,
+			sendVerificationRequest,
 		})
 	],
 })
